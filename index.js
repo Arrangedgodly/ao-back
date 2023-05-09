8a76772b47e34e553e3dd2059dce54f554ca7315
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2023-05-08 21:33:18 -0600

    refactor: getting the back end proxy started for the Printful integration

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const port = 3001; // Choose a different port from your client-side app
+
+app.use(cors()); // Enable CORS for all routes
+
+const baseUrl = 'https://api.printful.com';
+const apiKey = 'Q50RatjJJejYNTu0HXbYiBRt8JdGWINdMie00OBi';
+
+// Create a route to proxy the Printful API request
+app.get('/store/products', async (req, res) => {
+  try {
+    const response = await axios.get(`${baseUrl}/store/products`, {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching products:', error.message);
+    res.status(500).json({ message: 'Error fetching products' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
